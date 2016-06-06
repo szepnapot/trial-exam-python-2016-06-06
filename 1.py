798e7e6ad45446bf6a9a1d794af5701ec5cb5e82
Peter Lodri <silentpocok@gmail.com>
2016-06-06 09:29:44 +0200

    1 solution

--- 1.py
+++ 1.py
@@ -1,3 +1,13 @@
 # Create a function that takes a list as a parameter,
 # and returns a new list with all it's element value doubled.
 # It should raise an error if the parameter is not a list
+
+def doubler(lista):
+	try:
+		return [i*2 for i in lista]
+	except TypeError:
+		return "Input must be iterable! {} is not a valid parameter.".format(lista)
+
+
+print(doubler([1,2,3,4,5]))
+print(doubler(5))
