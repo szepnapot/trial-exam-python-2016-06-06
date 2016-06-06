1c595bd195be434845ca5ace87bc8a66d1f07ee2
Peter Lodri <silentpocok@gmail.com>
2016-06-06 10:43:35 +0200

    updated testcase, removed failing

--- test_4.py
+++ test_4.py
@@ -6,12 +6,18 @@ class GreeterTest(unittest.TestCase):
 	def test_empty_string(self):
 		self.assertEqual(greeter(''), 'Hello, Mr Nobody!')
 
-	def test_not_string_inputs(self):
-		self.assertFalse(greeter(123))
-
 	def test_extra_long_string(self):
 		long_name = 'Peeeeti' * 100
 		self.assertEqual(greeter(long_name), 'Hello, ' + long_name + '!')
+
+	def test_with_name(self):
+		self.assertEqual(greeter('Peter'), 'Hello, Peter!')
+
+	def test_with_digit_name(self):
+		self.assertEqual(greeter('P3t3r'), 'Hello, P3t3r!')
+
+	def test_with_uppercase_name(self):
+		self.assertEqual(greeter('PETER'), 'Hello, PETER!')
 
 if __name__ == '__main__':
 	unittest.main()
