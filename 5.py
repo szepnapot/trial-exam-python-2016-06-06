256364159249d9d77872c9c31c56535e3482f669
Peter Lodri <silentpocok@gmail.com>
2016-06-06 10:02:56 +0200

    pirate counter

--- 5.py
+++ 5.py
@@ -9,3 +9,10 @@ pirates = [
 # Write a function that takes any list that contains pirates as in the example,
 # And returns a list of names containing the pirates that has wooden leg and
 # more than 15 gold
+
+def pirate_count(pirate_data):
+	rich_handicap_pirates = [pirates['Name'] for pirates in pirate_data \
+													 if pirates['has_wooden_leg'] == True and pirates['gold'] >= 15]
+	return rich_handicap_pirates
+
+print(pirate_count(pirates))
